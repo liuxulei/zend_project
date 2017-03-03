4b78b1986b6588ce818d64144a7f438fa625c969
alvinxlliu <alvinxlliu@tencent.com>
2017-03-03 10:54:38 +0800

    change bmi calculate to angularjs

--- application/views/view.php
+++ application/views/view.php
@@ -2,15 +2,20 @@
 <html>
 <head>
 <meta charset="utf-8">
-<!-- <script src="/public/js/angular.min.js"></script> -->
+<script src="/public/js/angular.min.js"></script>
 </head>
-<body ng-app="app">
-<!-- <p>This is view view.</p> -->
+<body>
 <p>Test My AngularJS</p>
-<div ng-app="fashion">
-     <p>名字 : <input type="text" ng-model="name"></p>
-     <h1>Hello {{name}}</h1>
-     <h4>我的第一个表达式：{{ 5+5 }}</h4>
+<div ng-app="fashion" ng-controller="controller">
+	<p>身高(m)：<input type="text" ng-model="height"></p>
+	<p>体重(kg)：<input type="text" ng-model="weight"></p>
+	<!-- 
+	<h1>Hello {{name}}</h1>
+	<h4>你的身高是：{{height}}<br/>
+	你的体重是：{{weight}}<br/>
+	 -->
+	<button ng-click="toggle()">计算</button><br/>
+	你的bmi是：{{bmi}}，属于{{type}}</h4>
 </div>
 
 <div>
@@ -20,29 +25,32 @@
 <script>
 'use strict';
 
-var height = parseFloat(prompt('请输入身高(m):'));
-var weight = parseFloat(prompt('请输入体重(kg):'));
+var app = angular.module("fashion", []);
+app.controller('controller', function($scope){
+	$scope.name = '刘旭雷';
+	$scope.height = '1.75';
+	$scope.weight = '76';
+	$scope.bmi = 24.82;
+	$scope.type = '正常';
 
-var bmi = weight/Math.pow(height, 2);
-if(bmi <18.5){
-  alert('过轻');
-}else if(bmi < 25){
-  alert('正常');
-}else if(bmi<28){
-  alert('过重');
-}else if(bmi<32){
-  alert('肥胖');
-}else{
-  alert('严重肥胖');
-}
+	$scope.toggle = function(){
+		$scope.bmi = $scope.weight/Math.pow($scope.height, 2);
+		$scope.bmi = $scope.bmi.toFixed(2);
+		var bmi = $scope.bmi;
 
-
-var app = angular.module("app", []);
-app.directive("hello", function(){
-	return {
-		// restrict: 'A',
-		template : "Hello World!",
-		// replace : true
+		var type = '';
+		if(bmi <18.5){
+		  type = '过轻';
+		}else if(bmi < 25){
+		  type = '正常';
+		}else if(bmi<28){
+		  type = '过重';
+		}else if(bmi<32){
+		  type = '肥胖';
+		}else{
+		  type = '严重肥胖';
+		}
+		$scope.type = type;
 	};
 });
 
